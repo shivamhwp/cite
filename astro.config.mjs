ca52e3a9726e2c5c14609e09624d39ae7db1619d
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2024-09-15 16:29:41 +0530

    ahhhhh v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
