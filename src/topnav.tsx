cd20205fb338161f846a5a369374f850705a526f
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2024-12-25 13:08:56 +0530

    maybe

--- src/topnav.tsx
+++ src/topnav.tsx
@@ -1,22 +1,22 @@
-export function TopNav({ pathname }: { pathname: string }) {
+export function TopNav({ path }: { path: string }) {
   return (
     <div className="flex items-start justify-start gap-2 font-serif text-xl max-sm:pt-8">
       <a
         href="/"
-        className={`decoration-wavy underline-offset-4 transition duration-500 hover:underline ${pathname === "/" ? "underline decoration-[#f44747]" : ""} `}
+        className={`decoration-wavy underline-offset-4 transition duration-500 hover:underline ${path === "/" ? "underline decoration-[#f44747]" : ""} `}
       >
         home
       </a>
 
       <a
         href="/services"
-        className={`decoration-wavy underline-offset-4 transition duration-500 hover:underline ${pathname === "/services" ? "underline decoration-[#f44747]" : ""}`}
+        className={`decoration-wavy underline-offset-4 transition duration-500 hover:underline ${path === "/services" ? "underline decoration-[#f44747]" : ""}`}
       >
         services
       </a>
       <a
         href="/blogs"
-        className={`decoration-wavy underline-offset-4 transition duration-500 hover:underline ${pathname === "/blogs" ? "underline decoration-[#f44747]" : ""}`}
+        className={`decoration-wavy underline-offset-4 transition duration-500 hover:underline ${path === "/blogs" ? "underline decoration-[#f44747]" : ""}`}
       >
         blogs
       </a>
