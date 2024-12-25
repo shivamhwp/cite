67bac9251d5b83528b0e2c0de7ab5151e1b5a801
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2024-12-25 19:29:23 +0530

    idk man

--- src/topnav.tsx
+++ src/topnav.tsx
@@ -1,24 +1,26 @@
 export function TopNav({ path }: { path: string }) {
-  const activeVariant = "underline decoration-wavy underline-offset-4";
+  const normalVariant =
+    "decoration-[#f44747] transition duration-500 hover:underline hover:decoration-wavy hover:underline-offset-4";
+  const activeVariant = "!underline !decoration-wavy !underline-offset-4";
 
   return (
     <div className="flex items-start justify-start gap-2.5 font-serif text-xl max-sm:pt-8">
       <a
         href="/"
-        className={`decoration-[#f44747] transition duration-500 hover:underline hover:decoration-wavy hover:underline-offset-4 ${path === "/" ? activeVariant : " "} `}
+        className={` ${normalVariant} ${path === "/" ? activeVariant : " "} `}
       >
         home
       </a>
 
       <a
         href="/services"
-        className={`decoration-[#f44747] transition duration-500 hover:underline hover:decoration-wavy hover:underline-offset-4 ${path === "/services" ? activeVariant : " "} `}
+        className={`${normalVariant} ${path === "/services" ? activeVariant : " "} `}
       >
         services
       </a>
       <a
         href="/blogs"
-        className={`decoration-[#f44747] decoration-wavy transition duration-500 hover:underline hover:underline-offset-4 ${path === "/blogs" ? activeVariant : " "} `}
+        className={`${normalVariant} ${path === "/blogs" ? activeVariant : " "} `}
       >
         blogs
       </a>
