e26091363618aab92c6aff5fe60e5518eca80722
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2024-12-25 19:07:52 +0530

    [fixing] a minor bug

--- src/topnav.tsx
+++ src/topnav.tsx
@@ -1,22 +1,24 @@
 export function TopNav({ path }: { path: string }) {
+  const activeVariant = "underline decoration-wavy underline-offset-4";
+
   return (
-    <div className="flex items-start justify-start gap-2 font-serif text-xl max-sm:pt-8">
+    <div className="flex items-start justify-start gap-2.5 font-serif text-xl max-sm:pt-8">
       <a
         href="/"
-        className={`decoration-wavy underline-offset-4 transition duration-500 hover:underline ${path === "/" ? "underline decoration-[#f44747]" : ""} `}
+        className={`decoration-[#f44747] transition duration-500 hover:underline hover:decoration-wavy hover:underline-offset-4 ${path === "/" ? activeVariant : " "} `}
       >
         home
       </a>
 
       <a
         href="/services"
-        className={`decoration-wavy underline-offset-4 transition duration-500 hover:underline ${path === "/services" ? "underline decoration-[#f44747]" : ""}`}
+        className={`decoration-[#f44747] transition duration-500 hover:underline hover:decoration-wavy hover:underline-offset-4 ${path === "/services" ? activeVariant : " "} `}
       >
         services
       </a>
       <a
         href="/blogs"
-        className={`decoration-wavy underline-offset-4 transition duration-500 hover:underline ${path === "/blogs" ? "underline decoration-[#f44747]" : ""}`}
+        className={`decoration-[#f44747] decoration-wavy transition duration-500 hover:underline hover:underline-offset-4 ${path === "/blogs" ? activeVariant : " "} `}
       >
         blogs
       </a>
