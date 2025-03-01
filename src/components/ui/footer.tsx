b257a6f0b9f8f479c393bd484ee45e9e56c2f513
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2025-03-01 12:02:49 +0530

    minor changes

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -2,7 +2,7 @@ import { socials } from "../../data";
 
 function Footer() {
   return (
-    <div className="absolute bottom-0 flex h-28 w-full items-center justify-center bg-white max-sm:px-0">
+    <div className="flex h-16 w-full items-center justify-center bg-white">
       <div className="flex w-full items-center justify-around gap-4 border-t border-dashed border-black/30 px-24 py-4 max-sm:gap-2 max-sm:px-0 md:text-lg lg:text-lg xl:text-lg">
         {socials?.map((x) => (
           <a
